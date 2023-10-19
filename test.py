e778a0711dc738fb223fba0e53a52d87b9576c75
Ravi Kumar <human26072004@gmail.com>
2023-10-19 21:54:42 +0530

    added degrees

---
+++ test.py
@@ -0,0 +1,51 @@
+import csv
+
+people = {}
+
+names = {}
+
+movies = {}
+
+def load_data(directory):
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+load_data("small")
+
+for person in people: 
+    print(people[person]["movies"])
+    print("")
+print("")
+for name in names:
+    print("Names",names[name])
+    print("")
